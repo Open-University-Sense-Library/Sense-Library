cfff973cbcb1c5718e22ef2c951682d381a60d4f
ToranSharma <toransharma27@gmail.com>
2013-08-01 16:40:42 +0100

    Third commit

--- sense.py
+++ sense.py
@@ -1,30 +1,46 @@
-import serial
-import time
-import binascii
+import serial, time, binascii
 from ctypes import c_uint8
 
 def openSerialPort(port_num):
     print ("Opening Serial port...")
-    global ser
-    ser = serial.Serial(port_num, 115200,timeout=1 )
+    global SER
+    SER = serial.Serial(port_num, 115200,timeout=1 )
     time.sleep(2)
-    global command_header
-    command_header = b'\x54\xFE'
-    print ("Connected to sense at port: " + ser.name)
+    global COMMAND_HEADER
+    COMMAND_HEADER = b'\x54\xFE'
+    print ("Connected to sense at port: " + SER.name)
 
 
-def ledOn(led):
-    led_id=bytes(c_uint8(2**(led-1)))
-    ser.write(command_header + b'\xC1' + led_id)
-    reply=binascii.hexlify(ser.read(size=3))
+def pingSenseBoard():
+    byte_1 = b'\x00'
+    byte_2 = b'\x00'
+    SER.write(COMMAND_HEADER + byte_1 + byte_2)
+    reply=binascii.hexlify(SER.read(size=5))
+    return str(reply,'ascii')
+    
+
+def resetSenseBoard():
+    byte_1 = b'\x10'
+    byte_2 = b'\x00'
+    SER.write(COMMAND_HEADER + byte_1 + byte_2)
+    reply=binascii.hexlify(SER.read(size=5))
+    return str(reply,'ascii')
+    
+
+def ledOn(led_id):
+    byte_1 = b'\xC1'
+    byte_2 = bytes(c_uint8(2**(led_id-1)))
+    SER.write(COMMAND_HEADER + byte_1 + byte_2)
+    reply=binascii.hexlify(SER.read(size=3))
     led = 0
     return str(reply,'ascii')
     
 
-def ledOff(led):
-    led_id=bytes(c_unit8(2**(led-1)))
-    ser.write(command_header + b'\xC0' + led_id)
-    reply=binascii.hexlify(ser.read(size=3))
+def ledOff(led_id):
+    byte_1 = b'\xC0'
+    byte_2 = bytes(c_unit8(2**(led_id-1)))
+    SER.write(COMMAND_HEADER + byte_1 + byte_2)
+    reply=binascii.hexlify(SER.read(size=3))
     led = 0
     return str(reply,'ascii')
     
@@ -32,18 +48,32 @@ def ledOff(led):
 def stepperMove(motor_id, steps):
     byte_1 = bytes(c_uint8(240 + motor_id))
     byte_2 = bytes(c_uint8(steps))
-    ser.write(command_header + byte_1 + byte_2)
-    reply=binascii.hexlify(ser.read(size=3))
+    SER.write(COMMAND_HEADER + byte_1 + byte_2)
+    reply=binascii.hexlify(SER.read(size=3))
     return str(reply,'ascii')
     
 
-def dcMove(direction, speed, motor):
+def servoSetPosition(servo_id, angle):
+    byte_1 = bytes(c_uint8(208+ servo))
+    byte_2 = bytes(c_unit8(angle))
+    SER.write(COMMAND_HEADER + byte_1 + byte_2)
+    reply=binascii.hexlify(SER.read(size=3))
+    return str(reply,'ascii')
+
+
+def dcMove(direction, speed, motor_id):
     byte_1 = bytes(c_uint8( 128 + direction ))
-    byte_2 = bytes(c_uint8(speed*32 + motor))
-    ser.write(command_header + byte_1 + byte_2)
-    reply=binascii.hexlify(ser.read(size=3))
+    byte_2 = bytes(c_uint8(speed*32 + motor_id))
+    SER.write(COMMAND_HEADER + byte_1 + byte_2)
+    reply=binascii.hexlify(SER.read(size=3))
     return str(reply,'ascii')
     
 
-openSerialPort(4)
-dcMove(1, 5, 1)
+def dcOff(motor_id):
+    byte_1 = '\x80'
+    byte_2 = bytes(c_uint(motor_id))
+    SER.write(COMMAND_HEADER + byte_1 + byte_2)
+    reply=binascii.hexlify(SER.read(size=3))
+    return str(reply,'ascii')
+    
+
